6fe581236f7dde5f03eae20593c7454821ee8bdc
ipa-bnm <bnm@ipa.fraunhofer.de>
2013-01-10 17:47:27 +0100

    added checkbutton to enable command confirmation

--- cob_command_gui/ros/src/knoeppkes.py
+++ cob_command_gui/ros/src/knoeppkes.py
@@ -72,6 +72,7 @@ import sys
 
 planning_enabled = False
 base_diff_enabled = False
+confirm_commands_enabled = True
 
 initialized = False
 
@@ -82,15 +83,24 @@ gtk.gdk.threads_init()
 def start(func, args):
   global planning_enabled
   global base_diff_enabled
-  largs = list(args)
-  if(largs[0] == "arm"):
-    if(planning_enabled):
-      largs.append("planned")
-  if(largs[0] == "base"):
-    if(base_diff_enabled):
-  	largs.append("diff")	
-  #print "Args", tuple(largs)
-  thread.start_new_thread(func,tuple(largs))
+  global confirm_commands_enabled
+  execute_command = True
+
+  if confirm_commands_enabled:
+    confirm_dialog = gtk.MessageDialog(None, 0, gtk.MESSAGE_QUESTION, gtk.BUTTONS_YES_NO, "Execute Command?")
+    if confirm_dialog.run() == gtk.RESPONSE_NO:
+      execute_command = False
+    confirm_dialog.destroy()
+  if execute_command:
+    largs = list(args)
+    if(largs[0] == "arm"):
+      if(planning_enabled):
+        largs.append("planned")
+    if(largs[0] == "base"):
+      if(base_diff_enabled):
+     largs.append("diff") 
+    #print "Args", tuple(largs)
+    thread.start_new_thread(func,tuple(largs))
 
 def startGTK(widget, data):
   data()
@@ -142,8 +152,13 @@ class GtkGeneralPanel(gtk.Frame):
     base_mode_check = gtk.CheckButton("Base Diff")
     base_mode_check.connect("toggled", self.base_mode_toggle)
     self.vbox.pack_start(base_mode_check, False, False, 5)
+
+    confirm_com_check = gtk.CheckButton("Confirm Commands")
+    confirm_com_check.set_active(confirm_commands_enabled)
+    confirm_com_check.connect("toggled", self.confirm_com_toggle)
+    self.vbox.pack_start(confirm_com_check, False, False, 5)
     
-    but = gtk.Button(stock=gtk.STOCK_QUIT	)
+    but = gtk.Button(stock=gtk.STOCK_QUIT )
     but.connect("clicked", lambda w: gtk.main_quit())
     self.vbox.pack_start(but, False, False, 5)
     initialized = True
@@ -192,8 +207,14 @@ class GtkGeneralPanel(gtk.Frame):
     if(base_diff_enabled):
       base_diff_enabled = False
     else:
-      base_diff_enabled = True       
-		    
+      base_diff_enabled = True
+
+  def confirm_com_toggle(self, b):
+    global confirm_commands_enabled
+    if(confirm_commands_enabled):
+      confirm_commands_enabled = False
+    else:
+      confirm_commands_enabled = True
 
 ## Class for gtk panel implementation
 class GtkPanel(gtk.Frame):
@@ -228,7 +249,7 @@ class Knoeppkes():
   
     self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
     self.window.connect("delete_event", self.delete_event)
-    self.window.set_title("cob command gui")	
+    self.window.set_title("cob command gui")  
     self.window.set_size_request(1000, 500)
     vbox = gtk.VBox(False, 1)
     self.hbox = gtk.HBox(True, 10)
