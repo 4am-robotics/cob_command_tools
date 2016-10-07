df09b498ff43617e9a94974133ca4f251b06053d
Florian Weisshardt <florian.weisshardt@ipa.fraunhofer.de>
2016-10-07 14:21:00 +0200

    fix diagnostics output

--- cob_monitoring/src/hz_monitor.py
+++ cob_monitoring/src/hz_monitor.py
@@ -4,6 +4,7 @@ import sys
 
 import rospy
 import rostopic
+import copy
 
 from diagnostic_msgs.msg import DiagnosticArray, DiagnosticStatus, KeyValue
 
@@ -11,7 +12,6 @@ class HzTest():
     def __init__(self):
         rospy.init_node("hz_monitor")
 
-        self.message_received = False
         # get parameters
         try:
             # topic to test
@@ -23,48 +23,47 @@ class HzTest():
             # length of test
             self.window_size = float(rospy.get_param('~window_size', 100))
             # name for diagnostic message
-            self.diagnostic_name = rospy.get_param('~diagnostic_name')
-            self.diagnostic_name = self.diagnostic_name.replace('/','_')
+            self.diagnostics_name = rospy.get_param('~diagnostics_name')
+            self.diagnostics_name = self.diagnostics_name.replace('/','_')
         except KeyError as e:
             rospy.logerr('hztest not initialized properly. Parameter [%s] not set. debug[%s] debug[%s]'%(str(e), rospy.get_caller_id(), rospy.resolve_name(e.args[0])))
             sys.exit(1)
 
         self.pub_diagnostics = rospy.Publisher('~diagnostics', DiagnosticArray, queue_size = 1)
+        self.missing_topics = copy.deepcopy(self.topics)
 
     def run(self):
         r = rospy.Rate(1)
 
         # wait for first message
         while not rospy.is_shutdown():
-            real_topic_store = []
             for topic in self.topics:
                 msg_class, real_topic, _ = rostopic.get_topic_class(topic, blocking=False) #pause hz until topic is published
                 if real_topic:
-                    if real_topic not in real_topic_store:
-                        real_topic_store.append(real_topic)
-            if len(real_topic_store) == len(self.topics):
+                    if real_topic in self.missing_topics:
+                        self.missing_topics.remove(real_topic)
+            if len(self.missing_topics) == 0:
                 break
-            rospy.loginfo("hz monitor is waiting for first message to be published on %s."%topic)
+            rospy.loginfo("hz monitor is waiting for type of topics %s."%str(self.missing_topics))
             self.publish_diagnostics()
             r.sleep()
 
         # call rostopic hz
         #rt = rostopic.ROSTopicHz(self.window_size)
         rt_HZ_store = []
-        for r_topic in real_topic_store:
+        for topic in self.topics:
             rt = rostopic.ROSTopicHz(self.window_size)
-            rospy.Subscriber(r_topic, rospy.AnyMsg, rt.callback_hz)
+            rospy.Subscriber(topic, rospy.AnyMsg, rt.callback_hz)
             rt_HZ_store.append(rt)
-            print("subscribed to [%s]"%r_topic)
+            rospy.loginfo("subscribed to [%s]"%topic)
 
         # publish diagnostics continuously
-
         while not rospy.is_shutdown():
             #rt.print_hz() # taken from 'rostopic hz' (/opt/ros/indigo/lib/python2.7/dist-packages/rostopic/__init__.py)
             self.publish_diagnostics(rt_HZ_store)
             r.sleep()
 
-    def publish_diagnostics(self, rt_HZ_store = None):
+    def publish_diagnostics(self, rt_HZ_store = []):
         # set desired rates
         if self.hzerror:
             if isinstance(self.hzerror, float) or isinstance(self.hzerror, int):
@@ -81,10 +80,15 @@ class HzTest():
         array = DiagnosticArray()
         array.header.stamp = rospy.Time.now()
         hz_status = DiagnosticStatus()
-        hz_status.name = self.diagnostic_name
-        hz_status.values.append(KeyValue("topic", str(self.topics)))
+        hz_status.name = self.diagnostics_name
+        hz_status.values.append(KeyValue("topics", str(self.topics)))
+        hz_status.values.append(KeyValue("desired_rate", str(self.hz)))
+        hz_status.values.append(KeyValue("min_rate", str(min_rate)))
+        hz_status.values.append(KeyValue("max_rate", str(max_rate)))
+        hz_status.values.append(KeyValue("window_size", str(self.window_size)))
+        publishing_rate_error = True
         publishing_rate_error = False
-        rates_store = [] ## to display individual rates per topics
+        rates = []
         
         consolidated_error_messages = {} ## store and display consolidated erros messages for all the topics
         consolidated_error_messages.setdefault("never received message for topics", [])
@@ -92,15 +96,24 @@ class HzTest():
         consolidated_error_messages.setdefault("publishing rate is too low for topics", [])
         consolidated_error_messages.setdefault("publishing rate is too high for topics", [])
 
+        if len(rt_HZ_store) != len(self.topics):
+            hz_status.level = DiagnosticStatus.WARN
+            hz_status.message = "could not determine type for topics %s. Probably never published on that topics."%self.missing_topics
+            array.status.append(hz_status)
+            self.pub_diagnostics.publish(array)
+            return
+
         # calculate actual rates
         for rt, topic in zip(rt_HZ_store, self.topics):
             if not rt or not rt.times:
                 hz_status.level = DiagnosticStatus.ERROR
-                hz_status.values.append(KeyValue("rate", str(0.0)))
+                publishing_rate_error = True
+                rates.append(0.0)
                 consolidated_error_messages["never received message for topics"].append(topic)
             elif rt.msg_tn == rt.last_printed_tn:
                 hz_status.level = DiagnosticStatus.ERROR
-                hz_status.values.append(KeyValue("rate", str(0.0)))
+                publishing_rate_error = True
+                rates.append(0.0)
                 consolidated_error_messages["no messages anymore for topics"].append(topic)
             else:
                 with rt.lock: # calculation taken from /opt/ros/indigo/lib/python2.7/dist-packages/rostopic/__init__.py
@@ -108,7 +121,7 @@ class HzTest():
                     mean = sum(rt.times) / n
                     rate = 1./mean if mean > 0. else 0
                     rt.last_printed_tn = rt.msg_tn
-                rates_store.append("%.2f" % rate)
+                rates.append(round(rate, 2))
                 if min_rate and rate < min_rate:
                     hz_status.level = DiagnosticStatus.WARN
                     publishing_rate_error = True
@@ -124,26 +137,21 @@ class HzTest():
 
         if publishing_rate_error:
             message = ""
-            key = "publishing rate is too low for topics"
-            if len(consolidated_error_messages[key]) > 0:
-                message += key +" "+ str(consolidated_error_messages[key])
-            key = "publishing rate is too high for topics"
-            if len(consolidated_error_messages[key]) > 0:
-                message += ", "+key +" "+ str(consolidated_error_messages[key])
             key = "never received message for topics"
             if len(consolidated_error_messages[key]) > 0:
-                message += ", "+key +" "+ str(consolidated_error_messages[key])
+                message += key + " " + str(consolidated_error_messages[key]) + ", "
             key = "no messages anymore for topics"
             if len(consolidated_error_messages[key]) > 0:
-                message += ", "+key +" "+ str(consolidated_error_messages[key])
+                message += key + " " + str(consolidated_error_messages[key]) + ", "
+            key = "publishing rate is too low for topics"
+            if len(consolidated_error_messages[key]) > 0:
+                message += key + " " + str(consolidated_error_messages[key]) + ", "
+            key = "publishing rate is too high for topics"
+            if len(consolidated_error_messages[key]) > 0:
+                message += key + " " + str(consolidated_error_messages[key])
             hz_status.message = message
-        
 
-        hz_status.values.append(KeyValue("rate", str(rates_store)))
-        hz_status.values.append(KeyValue("desired_rate", str(self.hz)))
-        hz_status.values.append(KeyValue("min_rate", str(min_rate)))
-        hz_status.values.append(KeyValue("max_rate", str(max_rate)))
-        hz_status.values.append(KeyValue("window_size", str(self.window_size)))
+        hz_status.values.append(KeyValue("rates", str(rates)))
         array.status.append(hz_status)
         self.pub_diagnostics.publish(array)
 
