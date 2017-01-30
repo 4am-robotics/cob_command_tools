93381871aaccd75c885cfd0c5bfbe79ae29d408c
ipa-fxm <felix.messmer@ipa.fhg.de>
2017-01-30 16:16:24 +0100

    add auto_recover to new cob_helper_tools pkg

---
+++ cob_helper_tools/scripts/auto_recover.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+import rospy
+
+from cob_msgs.msg import EmergencyStopState
+from diagnostic_msgs.msg import DiagnosticArray
+
+from simple_script_server import *
+sss = simple_script_server()
+
+class AutoRecover():
+
+  def __init__(self):
+    self.components = rospy.get_param('~components', [])
+    self.em_state = 0
+    rospy.Subscriber("/emergency_stop_state", EmergencyStopState, self.em_cb)
+    rospy.Subscriber("/diagnostics_agg", DiagnosticArray, self.diagnostics_cb)
+    self.last_time_recover = rospy.Time.now()
+
+  def em_cb(self, msg):
+    if msg.emergency_state != self.em_state:
+      if msg.emergency_state == 0:
+        rospy.loginfo("auto_recover from scanner stop")
+        self.recover()
+      self.em_state = msg.emergency_state
+
+  def recover(self):
+    # call recover for all components
+    for component in self.components:
+      handle = sss.recover(component)
+    self.last_time_recover = rospy.Time.now()
+
+  # auto recover based on diagnostics
+  def diagnostics_cb(self, msg):
+    for status in msg.status:
+      if status.level > 1: # only recover on error, not on warning status
+        if "Actuators" in status.name and self.em_state == 0 and (rospy.Time.now() - self.last_time_recover) > rospy.Duration(10):
+          self.recover()
+
+if __name__ == "__main__":
+
+  rospy.init_node("auto_recover")
+  AR = AutoRecover()
+  rospy.loginfo("auto recover running")
+  rospy.spin()
