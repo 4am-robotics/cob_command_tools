51969b96c80b4ba43aef7c0ee8a3d5cc7dc05cf1
ipa-fxm <fxm@ipa.fhg.de>
2017-02-17 08:33:07 +0000

    update psutil api

--- cob_monitoring/src/cpu_monitor.py
+++ cob_monitoring/src/cpu_monitor.py
@@ -535,7 +535,10 @@ class CPUMonitor():
         self._load1_threshold = rospy.get_param('~load1_threshold', 5.0)
         self._load5_threshold = rospy.get_param('~load5_threshold', 3.0)
 
-        self._num_cores = rospy.get_param('~num_cores', psutil.NUM_CPUS)
+        if versiontuple(psutil.__version__) < versiontuple('2.0.0'):
+            self._num_cores = rospy.get_param('~num_cores', psutil.NUM_CPUS)
+        else:
+            self._num_cores = rospy.get_param('~num_cores', psutil.cpu_count())
 
         self._temps_timer = None
         self._usage_timer = None
