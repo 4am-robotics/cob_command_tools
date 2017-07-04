b6ae74f432f55a8d92380d8a007b22085f4e455e
MattiaRacca <mattia.rh@gmail.com>
2017-07-04 09:50:50 +0200

    Remove delay feature. Add latched behaviour (True/False).

--- generic_throttle/src/generic_throttle/generic_throttle.py
+++ generic_throttle/src/generic_throttle/generic_throttle.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 
 import rospy
-import roslib
 import rostopic
 from threading import Lock
 from threading import currentThread
@@ -10,27 +9,37 @@ from functools import partial
 
 class GenericThrottle:
     def __init__(self):
-        self.namespace = None
         self.topic_dictionary = None
-        self.topic_framerate = None
-        self.delay = None
+        self.parameters = {'namespace': None,
+                           'topic_framerate': None,
+                           'latched': True,
+                           'lazy': False}
 
         rospy.init_node('generic_throttle')
         rospy.on_shutdown(self._shutdown)
 
-        # Read /generic_throttle/* parameters from server
-        parameter_list = ['namespace', 'topic_framerate', 'delay']
+        # Read mandatory /generic_throttle/* parameters from server
+        parameter_list = ['namespace', 'topic_framerate']
         for parameter_name in parameter_list:
             parameter_string = '/generic_throttle/' + parameter_name
             if rospy.has_param(parameter_string):
-                parameter = rospy.get_param(parameter_string)
-                setattr(self, parameter_name, parameter)
+                self.parameters[parameter_name] = \
+                    rospy.get_param(parameter_string)
             else:
                 rospy.logerr('Parameter ' + parameter_string + ' not available')
                 exit(5)
 
+        # Read optional /generic_throttle/* parameters from server
+        parameter_list = ['latched', 'lazy']
+        for parameter_name in parameter_list:
+            parameter_string = '/generic_throttle/' + parameter_name
+            if rospy.has_param(parameter_string):
+                self.parameters[parameter_name] = \
+                    rospy.get_param(parameter_string)
+
         # Check if each entry of topic_framerate has 2 entries
-        size_flag = all(len(item) == 2 for item in self.topic_framerate)
+        size_flag = all(len(item) == 2 for item in
+                        self.parameters['topic_framerate'])
 
         if(not(size_flag)):
             rospy.logerr('Parameter /generic_throttle/framerate must be'
@@ -42,9 +51,9 @@ class GenericThrottle:
         rospy.spin()
 
     def timer_callback(self, event, topic_id):
-
-        locking = self.topic_dictionary[topic_id]['lock'].acquire_lock(False)
         # The False argument is for a non blocking call
+        locking = self.topic_dictionary[topic_id]['lock'].acquire_lock(False)
+
 
         if not(locking):
             current_t = currentThread()
@@ -56,7 +65,7 @@ class GenericThrottle:
         subscriber = self.topic_dictionary[topic_id]['subscriber']
 
         # Check if pub and sub already exist, if not try to create them after
-        # checking the rostopic
+        # checking the ros topic
 
         if None in(publisher, subscriber):
             topic_info = rostopic.get_topic_class(topic_id)
@@ -68,10 +77,10 @@ class GenericThrottle:
             else:
                 # Create publisher
                 self.topic_dictionary[topic_id]['publisher'] = \
-                    rospy.Publisher(self.namespace + topic_id, topic_info[0],
-                                    queue_size=1)
-                rospy.loginfo('Created publisher for ' + self.namespace +
-                              topic_id)
+                    rospy.Publisher(self.parameters['namespace'] + topic_id,
+                                    topic_info[0], queue_size=1)
+                rospy.loginfo('Created publisher for ' +
+                              self.parameters['namespace'] + topic_id)
                 # Create subscriber
                 subscriber_partial = partial(self.subscriber_callback,
                                              topic_id=topic_id)
@@ -84,27 +93,17 @@ class GenericThrottle:
                 return
 
         last_message = self.topic_dictionary[topic_id]['last_message']
-        if last_message is None:
-            rospy.logwarn('No message available for ' + topic_id + ' yet. ' +
-                          'Sleep for ' + str(self.delay) + ' seconds.')
-            self.topic_dictionary[topic_id]['lock'].release_lock()
-            rospy.sleep(self.delay)
-            return
-        else:
-            last_timestamp = self.topic_dictionary[topic_id]['last_timestamp']
-            if rospy.Time.now() - last_timestamp > rospy.Duration(self.delay):
-                rospy.logwarn('Last message older than ' + str(self.delay)
-                              + ' seconds. Discard last message')
+
+        if last_message is not None:
+            self.topic_dictionary[topic_id]['publisher'].publish(
+                last_message)
+            if not(self.parameters['latched']):
                 self.topic_dictionary[topic_id]['last_message'] = None
-                # self.topic_dictionary[topic_id]['publisher'] = None
-                # self.topic_dictionary[topic_id]['subscriber'] = None
-                self.topic_dictionary[topic_id]['lock'].release_lock()
-                return
-            else:
-                self.topic_dictionary[topic_id]['publisher'].publish(
-                    last_message)
-                self.topic_dictionary[topic_id]['lock'].release_lock()
-                return
+            self.topic_dictionary[topic_id]['lock'].release_lock()
+            return
+
+        self.topic_dictionary[topic_id]['lock'].release_lock()
+        return
 
     def subscriber_callback(self, data, topic_id):
         locking = self.topic_dictionary[topic_id]['lock'].acquire_lock(False)
@@ -123,9 +122,6 @@ class GenericThrottle:
             self.topic_dictionary[topic_id]['last_timestamp'] = \
                 rospy.Time.now()
 
-        if self.topic_dictionary[topic_id]['last_message'] is None:
-            rospy.loginfo('First message received for topic ' + topic_id)
-
         self.topic_dictionary[topic_id]['last_message'] = data
         self.topic_dictionary[topic_id]['lock'].release_lock()
 
@@ -138,7 +134,8 @@ class GenericThrottle:
                                        'last_timestamp': None,
                                        'subscriber': None,
                                        'publisher': None, 'lock': Lock()}
-                                 for (key, value) in self.topic_framerate}
+                                 for (key, value) in
+                                 self.parameters['topic_framerate']}
 
         for key, element in self.topic_dictionary.iteritems():
             element['lock'].acquire_lock()
