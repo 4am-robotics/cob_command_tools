5b54d85b6f1944a02b8214c2876aed5f2b20fa3e
ipa-uhr-mk <uhr-mk@ipa.fhg.de>
2017-07-18 17:58:51 +0200

    use license apache 2.0

--- cob_interactive_teleop/include/cob_interactive_teleop/topics_list.h
+++ cob_interactive_teleop/include/cob_interactive_teleop/topics_list.h
@@ -1,29 +1,19 @@
-/******************************************************************************
- * \file
+/*
+ * Copyright 2017 Fraunhofer Institute for Manufacturing Engineering and Automation (IPA)
  *
- * $Id:$
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Copyright (C) Brno University of Technology
- *
- * This file is part of software developed by dcgm-robotics@FIT group.
- *
- * Author: Michal Spanel (spanel@fit.vutbr.cz)
- * Supervised by: Michal Spanel (spanel@fit.vutbr.cz)
- * Date: 02/06/2012
- *
- * This file is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This file is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this file.  If not, see <http://www.gnu.org/licenses/>.
+ *   http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 
 #ifndef COB_INTERACTIVE_TELEOP_TOPICS_LIST_H
 #define COB_INTERACTIVE_TELEOP_TOPCIS_LIST_H
