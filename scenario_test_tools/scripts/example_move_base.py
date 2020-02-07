6cd8e3c372ea420093ee70fa97401b050179415a
Loy van Beek <loy.vanbeek@mojin-robotics.de>
2020-02-07 17:15:50 +0100

    Add example move base script

---
+++ scenario_test_tools/scripts/example_move_base.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+
+import rospy
+
+from scenario_test_tools.scriptable_move_base import ScriptableMoveBase
+
+from move_base_msgs.msg import MoveBaseAction, MoveBaseResult
+
+
+if __name__ == "__main__":
+    rospy.init_node('move_base_fake')
+
+    succeeded = MoveBaseResult()
+
+    move_base = ScriptableMoveBase('/move_base', MoveBaseAction, result_delay=5)
+    move_base.start()
+    rospy.loginfo("fake move base running")
+
+    move_base.reply(succeeded, marker='1: succeeded')
+
+    # To check that the client will perhaps retry the navigation or otherwise handle an abort
+    move_base.reply(move_base.ABORT_GOAL, marker='2: Aborted')
+
+    # The client will have to decide it's taking too long and cancel
+    move_base.reply(move_base.IGNORE_GOAL, marker='3: Ignored')
+
+    rospy.spin()
