6883d136e11e5ecc62892e1709a038d4fa650bb6
fmessmer <felixmessmer@gmail.com>
2020-02-07 11:46:59 +0100

    refactor hz_monitor

--- cob_monitoring/src/hz_monitor.py
+++ cob_monitoring/src/hz_monitor.py
@@ -40,56 +40,41 @@ class HzTest():
             # name for diagnostic message
             self.diagnostics_name = rospy.get_param('~diagnostics_name',"")
             self.diagnostics_name = self.diagnostics_name.replace('/','_')
-            # sampling rate
-            self.sampling_rate = rospy.get_param('~sampling_rate', 1)
-            if self.hz < 2*self.sampling_rate:
-                rospy.logwarn("sampling_rate is probably to low: desired hz rate should at least be double the sampling_rate. (desired hz rate: %.2f, sampling_rate: %.2f)",self.hz, self.sampling_rate)
+            self.diagnostics_rate = 1.0
         except KeyError as e:
             rospy.logerr('hztest not initialized properly. Parameter [{}] not set. debug[{}] debug[{}]'.format(e, rospy.get_caller_id(), rospy.resolve_name(e.args[0])))
             sys.exit(1)
 
-        self.pub_diagnostics = rospy.Publisher('~diagnostics', DiagnosticArray, queue_size = 1)
+        self.rt_HZ_store = dict()
         self.missing_topics = copy.deepcopy(self.topics)
+        self.pub_diagnostics = rospy.Publisher('~diagnostics', DiagnosticArray, queue_size = 1)
+        self.timer_diagnostics = rospy.Timer(rospy.Duration(self.diagnostics_rate), self.publish_diagnostics)
+
 
     def run(self):
-        r = rospy.Rate(self.sampling_rate)
-
         # wait for first message
-        while not rospy.is_shutdown():
+        while len(self.missing_topics) != 0 and not rospy.is_shutdown():
             for topic in self.topics:
-                msg_class, real_topic, _ = rostopic.get_topic_class(topic, blocking=False) #pause hz until topic is published
+                msg_class, real_topic, msg_eval = rostopic.get_topic_class(topic, blocking=False) #pause hz until topic is published
                 if real_topic:
                     if real_topic in self.missing_topics:
                         self.missing_topics.remove(real_topic)
-            if len(self.missing_topics) == 0:
-                break
-            rospy.logdebug("hz monitor is waiting for type of topics {}.".format(self.missing_topics))
-            self.publish_diagnostics()
-            
+
             try:
-                r.sleep()
+                rospy.logdebug("hz monitor is waiting for type of topics {}.".format(self.missing_topics))
+                rospy.Rate(self.diagnostics_rate).sleep()
             except rospy.exceptions.ROSInterruptException:
                 pass
 
         # call rostopic hz
-        #rt = rostopic.ROSTopicHz(self.window_size)
-        rt_HZ_store = []
+        self.rt_HZ_store = dict()
         for topic in self.topics:
             rt = rostopic.ROSTopicHz(self.window_size)
             rospy.Subscriber(topic, rospy.AnyMsg, rt.callback_hz)
-            rt_HZ_store.append(rt)
+            self.rt_HZ_store[topic] = rt
             rospy.loginfo("subscribed to [{}]".format(topic))
 
-        # publish diagnostics continuously
-        while not rospy.is_shutdown():
-            #rt.print_hz() # taken from 'rostopic hz' (/opt/ros/indigo/lib/python2.7/dist-packages/rostopic/__init__.py)
-            self.publish_diagnostics(rt_HZ_store)
-            try:
-                r.sleep()
-            except rospy.exceptions.ROSInterruptException:
-                pass
-
-    def publish_diagnostics(self, rt_HZ_store = []):
+    def publish_diagnostics(self, event):
         # set desired rates
         if self.hzerror:
             if isinstance(self.hzerror, float) or isinstance(self.hzerror, int):
@@ -107,12 +92,13 @@ class HzTest():
         array.header.stamp = rospy.Time.now()
         hz_status = DiagnosticStatus()
         hz_status.name = self.diagnostics_name
+        hz_status.level = DiagnosticStatus.OK
+        hz_status.message = 'all publishing rates are ok'
         hz_status.values.append(KeyValue("topics", str(self.topics)))
         hz_status.values.append(KeyValue("desired_rate", str(self.hz)))
         hz_status.values.append(KeyValue("min_rate", str(min_rate)))
         hz_status.values.append(KeyValue("max_rate", str(max_rate)))
         hz_status.values.append(KeyValue("window_size", str(self.window_size)))
-        publishing_rate_error = False
         rates = []
         
         consolidated_error_messages = {} ## store and display consolidated erros messages for all the topics
@@ -121,23 +107,29 @@ class HzTest():
         consolidated_error_messages.setdefault("publishing rate is too low for topics", [])
         consolidated_error_messages.setdefault("publishing rate is too high for topics", [])
 
-        if len(rt_HZ_store) != len(self.topics):
+        if not all (k in self.rt_HZ_store for k in self.topics):
             hz_status.level = DiagnosticStatus.WARN
-            hz_status.message = "could not determine type for topics %s. Probably never published on that topics."%self.missing_topics
+            hz_status.message = "could not determine type for topics {}. Probably never published on that topics.".format(self.missing_topics)
             array.status.append(hz_status)
             self.pub_diagnostics.publish(array)
             return
 
         # calculate actual rates
-        for rt, topic in zip(rt_HZ_store, self.topics):
+        for topic, rt in self.rt_HZ_store.items():
+            #print("rt.times {}".format(rt.times))
+            #print("rt.msg_tn {}".format(rt.msg_tn))
+            #print("rt.last_printed_tn {}".format(rt.last_printed_tn))
+            #print("rt.delta: {}".format(rt.msg_tn - rt.last_printed_tn))
+            #print("event.current_real: {}".format(event.current_real.to_sec()))
+            #print("event.last_real: {}".format(event.last_real.to_sec()))
+            #print("event.delta: {}".format((event.current_real - event.last_real).to_sec()))
             if not rt or not rt.times:
                 hz_status.level = DiagnosticStatus.ERROR
-                publishing_rate_error = True
                 rates.append(0.0)
                 consolidated_error_messages["never received message for topics"].append(topic)
-            elif rt.msg_tn == rt.last_printed_tn:
+            elif rt.msg_tn == rt.last_printed_tn \
+                 and not (event.current_real.to_sec() < rt.msg_tn + 1/self.hz):  # condition to prevent WARN for topics with hz<diagnostics_rate
                 hz_status.level = DiagnosticStatus.ERROR
-                publishing_rate_error = True
                 rates.append(0.0)
                 consolidated_error_messages["no messages anymore for topics"].append(topic)
             else:
@@ -149,18 +141,13 @@ class HzTest():
                 rates.append(round(rate, 2))
                 if min_rate and rate < min_rate:
                     hz_status.level = DiagnosticStatus.WARN
-                    publishing_rate_error = True
                     consolidated_error_messages["publishing rate is too low for topics"].append(topic)
-                elif max_rate and rate > max_rate:
-                    hz_status.level = DiagnosticStatus.WARN
-                    publishing_rate_error = True
-                    consolidated_error_messages["publishing rate is too high for topics"].append(topic)
-                else:
-                    if not publishing_rate_error:
-                        hz_status.level = DiagnosticStatus.OK
-                        hz_status.message = 'all publishing rates are ok'
 
-        if publishing_rate_error:
+                if max_rate and rate > max_rate:
+                    hz_status.level = DiagnosticStatus.WARN
+                    consolidated_error_messages["publishing rate is too high for topics"].append(topic)
+
+        if hz_status.level != DiagnosticStatus.OK:
             message = ""
             key = "never received message for topics"
             if len(consolidated_error_messages[key]) > 0:
@@ -183,3 +170,4 @@ class HzTest():
 if __name__ == '__main__':
     hzt = HzTest()
     hzt.run()
+    rospy.spin()
