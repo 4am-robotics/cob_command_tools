98ca85f728e1fad2b1de67290648de454fb7397c
hyb <hannes.bachter@unity-robotics.de>
2020-02-11 13:39:19 +0100

    get timeout param

--- scenario_test_tools/scripts/example_move_base.py
+++ scenario_test_tools/scripts/example_move_base.py
@@ -14,19 +14,21 @@ if __name__ == "__main__":
 
     # By default, wait 5 secs for the robot to arrive
     result_delay = rospy.get_param("result_delay", 5)
+    # By default, wait forever for a move_base goal
+    timeout = rospy.get_param("timeout", None)
 
     move_base = ScriptableMoveBase(rospy.get_name(), MoveBaseAction, default_result_delay=result_delay)
     move_base.start()
     rospy.loginfo("fake move base running")
 
     try:
-        move_base.reply(succeeded, marker='1: succeeded', reply_delay=3, timeout=60)
+        move_base.reply(succeeded, marker='1: succeeded', reply_delay=3, timeout=timeout)
 
         # To check that the client will perhaps retry the navigation or otherwise handle an abort
-        move_base.reply(move_base.ABORT_GOAL, marker='2: Aborted')
+        move_base.reply(move_base.ABORT_GOAL, marker='2: Aborted', timeout=timeout)
 
         # The client will have to decide it's taking too long and cancel
-        move_base.reply(move_base.IGNORE_GOAL, marker='3: Ignored', reply_delay=0)
+        move_base.reply(move_base.IGNORE_GOAL, marker='3: Ignored', reply_delay=0, timeout=timeout)
     except AssertionError as assertion_err:
         rospy.logerr(assertion_err)
         raise assertion_err
