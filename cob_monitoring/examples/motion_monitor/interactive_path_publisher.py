181305fcc969eb57afc79f30fbffbef75a415882
Daniel Azanov <sirian.94@mail.ru>
2023-06-28 14:46:32 +0000

    [motion_monitor] timer instead of spin, more params into yaml

--- cob_monitoring/examples/motion_monitor/interactive_path_publisher.py
+++ cob_monitoring/examples/motion_monitor/interactive_path_publisher.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import signal
 import subprocess
 
 import rospy
 import tf2_ros
-from geometry_msgs.msg import Pose, PoseStamped, TransformStamped, Vector3
+from geometry_msgs.msg import Pose, PoseStamped, TransformStamped, Vector3, Quaternion
 from interactive_markers.interactive_marker_server import InteractiveMarkerServer
 from nav_msgs.msg import Path
 from std_msgs.msg import ColorRGBA
@@ -17,7 +17,7 @@ from visualization_msgs.msg import (
 class InteractivePathPublisher():
     def __init__(self):
         self.tf_broadcaster = tf2_ros.TransformBroadcaster()
-        map_base_link_xyz_quat = rospy.get_param("~map_base_link_xyz_quat")
+        path_target_xyz_quat = rospy.get_param("~path_target_xyz_quat")
 
         self.path_topic = rospy.get_param("~path_topic", "/path")
         self.path_pub = rospy.Publisher(self.path_topic, Path, queue_size=1)
@@ -26,6 +26,7 @@ class InteractivePathPublisher():
         self.path_frame_id = rospy.get_param("~path_frame_id", "map")
         self.path = self.get_path(self.path_frame_id)
 
+        # interactive marker
         interactive_marker_server = rospy.get_param(
             "~interactive_marker_server", "interactive_marker"
         )
@@ -33,34 +34,42 @@ class InteractivePathPublisher():
         interactive_marker_description = rospy.get_param(
             "~interactive_marker_description", "Drag to create path"
         )
+        interactive_marker_scale_xyz = rospy.get_param(
+            "~interactive_marker_scale_xyz", [1.5, 0.2, 0.2]
+        )
+        interactive_marker_color_rgba = rospy.get_param(
+            "~interactive_marker_color_rgba", [0, 1, 0, 1]
+        )
+        interactive_marker_orientation_quat = rospy.get_param(
+            "~interactive_marker_orientation_quat", [0, 0.7071, 0, 0.7071]
+        )
         self.server = InteractiveMarkerServer(interactive_marker_server)
         self.init_interactive_marker(
-            interactive_marker_name, interactive_marker_description, self.path_frame_id,
+            name=interactive_marker_name,
+            description=interactive_marker_description,
+            frame_id=self.path_frame_id,
+            scale=Vector3(*interactive_marker_scale_xyz),
+            color=ColorRGBA(*interactive_marker_color_rgba),
+            orientation=Quaternion(*interactive_marker_orientation_quat),
             init_position=Vector3(
-                0.9 * map_base_link_xyz_quat[0],
-                0.9 * map_base_link_xyz_quat[1],
+                0.9 * path_target_xyz_quat[0],
+                0.9 * path_target_xyz_quat[1],
                 0.2
             )
         )
 
-        self.tf_path_target = self.get_tf_map_base_link(
-            self.path_frame_id, self.target_frame_id, map_base_link_xyz_quat
+        self.tf_path_target = InteractivePathPublisher.get_transform_from_pose_xyz_quat(
+            self.path_frame_id, self.target_frame_id, path_target_xyz_quat
         )
 
-    def get_path(self, path_frame_id: str):
-        path = Path()
-        path.header.frame_id = path_frame_id
-        return path
-
     def init_interactive_marker(
-        self, interactive_marker_name: str, interactive_marker_description: str, frame_id: str,
-        init_position: Vector3,
-        scale: Vector3 = Vector3(0.4, 0.4, 0.4), color: ColorRGBA = ColorRGBA(0, 1, 0, 1)
-    ):
+        self, name: str, description: str, frame_id: str, init_position: Vector3,
+        scale: Vector3, color: ColorRGBA, orientation: Quaternion
+    ) -> None:
         int_marker = InteractiveMarker()
         int_marker.header.frame_id = frame_id
-        int_marker.name = interactive_marker_name
-        int_marker.description = interactive_marker_description
+        int_marker.name = name
+        int_marker.description = description
         int_marker.pose.position = init_position
 
         marker = Marker()
@@ -71,21 +80,18 @@ class InteractivePathPublisher():
         control = InteractiveMarkerControl()
         control.always_visible = True
         control.markers.append(marker)
-        control.orientation.w = 1
-        control.orientation.x = 0
-        control.orientation.y = 1
-        control.orientation.z = 0
+        control.orientation = orientation
         control.interaction_mode = InteractiveMarkerControl.MOVE_PLANE
         int_marker.controls.append(control)
 
         self.server.insert(int_marker, self.marker_callback)
         self.server.applyChanges()
 
-    def marker_callback(self, feedback: InteractiveMarkerFeedback):
+    def marker_callback(self, feedback: InteractiveMarkerFeedback) -> None:
         if feedback.event_type == InteractiveMarkerFeedback.POSE_UPDATE:
             self.publish_path(feedback.pose)
 
-    def publish_path(self, target_pose: Pose):
+    def publish_path(self, target_pose: Pose) -> None:
         pose = PoseStamped()
         pose.header.frame_id = self.path.header.frame_id
         pose.header.stamp = rospy.Time.now()
@@ -94,26 +100,28 @@ class InteractivePathPublisher():
         self.path.poses = [pose]
         self.path_pub.publish(self.path)
 
-    def get_tf_map_base_link(
-            self, frame_id: str, child_frame_id: str, map_base_link_xyz_quat: list
-    ):
+    def run(self) -> None:
+        now = rospy.Time.now()
+        self.tf_path_target.header.stamp = now
+        self.tf_broadcaster.sendTransform([self.tf_path_target])
+
+    @staticmethod
+    def get_path(path_frame_id: str) -> Path:
+        path = Path()
+        path.header.frame_id = path_frame_id
+        return path
+
+    @staticmethod
+    def get_transform_from_pose_xyz_quat(
+            frame_id: str, child_frame_id: str, xyz_quat: list
+    ) -> TransformStamped:
         transform = TransformStamped()
         transform.header.stamp = rospy.Time.now()
         transform.header.frame_id = frame_id
         transform.child_frame_id = child_frame_id
-        transform.transform.translation.x = map_base_link_xyz_quat[0]
-        transform.transform.translation.y = map_base_link_xyz_quat[1]
-        transform.transform.translation.z = map_base_link_xyz_quat[2]
-        transform.transform.rotation.x = map_base_link_xyz_quat[3]
-        transform.transform.rotation.y = map_base_link_xyz_quat[4]
-        transform.transform.rotation.z = map_base_link_xyz_quat[5]
-        transform.transform.rotation.w = map_base_link_xyz_quat[6]
+        transform.transform.translation = Vector3(*xyz_quat[:3])
+        transform.transform.rotation = Quaternion(*xyz_quat[3:7])
         return transform
-
-    def publish_test_data(self):
-        now = rospy.Time.now()
-        self.tf_path_target.header.stamp = now
-        self.tf_broadcaster.sendTransform([self.tf_path_target])
 
 
 if __name__ == "__main__":
@@ -129,14 +137,14 @@ if __name__ == "__main__":
     print(f"rosbag_play_cmd: {rosbag_play_cmd}")
 
     node = InteractivePathPublisher()
-    test_data_publish_rate = rospy.get_param("~test_data_publish_rate", 1)
-    rate = rospy.Rate(test_data_publish_rate)
+    publish_rate = rospy.get_param("~publish_rate", 1)
+    rate = rospy.Rate(publish_rate)
 
     rosbag_process = subprocess.Popen(rosbag_play_cmd)
 
     try:
         while not rospy.is_shutdown():
-            node.publish_test_data()
+            node.run()
             rate.sleep()
     except Exception as e:
         print(f"Exception caught: {e}")
--- cob_monitoring/src/motion_monitor.py
+++ cob_monitoring/src/motion_monitor.py
@@ -1,16 +1,17 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import math
 from dataclasses import dataclass
+import threading
 from typing import List, Tuple
 
 import rospy
 import tf2_ros
-from geometry_msgs.msg import Pose, Quaternion, Vector3
+from geometry_msgs.msg import Point, Pose, Quaternion, Vector3
 from nav_msgs.msg import Path
 from std_msgs.msg import ColorRGBA
 from tf2_geometry_msgs import do_transform_pose
-from tf.transformations import euler_from_quaternion, quaternion_from_euler
+from tf.transformations import quaternion_from_euler
 from visualization_msgs.msg import Marker
 
 
@@ -24,29 +25,49 @@ class MotionCategory:
 
 class MotionMonitor():
     def __init__(self):
-        self.path_topic = rospy.get_param("~path_topic", "/path")
+        self.tf_buffer = tf2_ros.Buffer()
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
+
         self.distance_threshold_m = rospy.get_param("~distance_threshold_m", 2.0)
         self.angle_threshold_rad = rospy.get_param(
             "~angle_threshold_rad", math.pi/6)  # 30 degrees in radians
         self.target_frame_id = rospy.get_param("~target_frame_id", "base_link")
 
+        # marker related params
+        self.should_publish_marker = rospy.get_param("~should_publish_marker", False)
         motion_monitor_marker_topic = rospy.get_param(
             "~motion_monitor_marker_topic", "/motion_monitor/motion_monitor_marker"
         )
-        self.direction_marker = MotionMonitor.get_direction_marker(self.target_frame_id)
+        marker_position_xyz = rospy.get_param("~marker_position_xyz", [0, 0, 0.1])
+        marker_scale_xyz = rospy.get_param("~marker_scale_xyz", [1.5, 0.2, 0.2])
+        self.direction_marker = MotionMonitor.get_direction_marker(
+            frame_id=self.target_frame_id,
+            scale=Vector3(*marker_scale_xyz),
+            positon=Point(*marker_position_xyz)
+        )
         self.marker_pub = rospy.Publisher(motion_monitor_marker_topic, Marker, queue_size=10)
-
-        self.tf_buffer = tf2_ros.Buffer()
-        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
-
-        rospy.Subscriber(self.path_topic, Path, self.path_callback, queue_size=1)
 
         self.categories = MotionMonitor.get_motion_categories(self.angle_threshold_rad)
 
+        self.last_path = None
+        self.last_path_mutex = threading.Lock()
+        self.max_path_age = rospy.get_param("~max_path_age", 1.0)
+        process_path_rate = rospy.get_param("~process_path_rate", 1.0)
+        self.path_topic = rospy.get_param("~path_topic", "/path")
+        rospy.Subscriber(self.path_topic, Path, self.path_callback, queue_size=1)
+        self.process_path_timer = rospy.Timer(
+            rospy.Duration(1.0 / process_path_rate), self.process_path
+        )
+
+    def path_callback(self, path: Path):
+        with self.last_path_mutex:
+            self.last_path = path
+
     def publish_direction_marker(self, color: ColorRGBA, yaw: float):
-        self.direction_marker.color = color
-        self.direction_marker.pose.orientation = Quaternion(*quaternion_from_euler(0, 0, yaw))
-        self.marker_pub.publish(self.direction_marker)
+        if self.should_publish_marker:
+            self.direction_marker.color = color
+            self.direction_marker.pose.orientation = Quaternion(*quaternion_from_euler(0, 0, yaw))
+            self.marker_pub.publish(self.direction_marker)
 
     def get_transform(self, target_frame: str, source_frame: str):
         try:
@@ -69,30 +90,42 @@ class MotionMonitor():
                 return category
         return None
 
-    def path_callback(self, path: Path):
-        transform_target_path = self.get_transform(self.target_frame_id, path.header.frame_id)
+    def process_path(self, _: rospy.timer.TimerEvent):
+        with self.last_path_mutex:
+            if not self.last_path:
+                return
 
-        for pose in path.poses:
-            # Transform pose to target frame
-            pose_in_target_frame = do_transform_pose(pose, transform_target_path)
+            path_age = rospy.Time.now() - self.last_path.header.stamp
+            if path_age > rospy.Duration(self.max_path_age):
+                return
 
-            distance = MotionMonitor.get_distance(pose_in_target_frame.pose)
-            if distance > self.distance_threshold_m:
-                target_yaw = math.atan2(pose_in_target_frame.pose.position.y,
-                                        pose_in_target_frame.pose.position.x)
-
-                rospy.loginfo(f"distance: {distance}, target_yaw: {target_yaw}")
-
-                category = self.get_valid_category(target_yaw)
-                if category:
-                    rospy.loginfo(f"Motion category: {category.name}")
-                    self.publish_direction_marker(category.color, category.yaw)
-                    break
-        else:
-            rospy.loginfo(
-                "No pose found in the path which is more than the threshold distance away "
-                f"({self.distance_threshold_m:.2f}m)"
+            transform_target_path = self.get_transform(
+                self.target_frame_id, self.last_path.header.frame_id
             )
+            if not transform_target_path:
+                return
+
+            for pose in self.last_path.poses:
+                # Transform pose to target frame
+                pose_in_target_frame = do_transform_pose(pose, transform_target_path)
+
+                distance = MotionMonitor.get_distance(pose_in_target_frame.pose)
+                if distance > self.distance_threshold_m:
+                    target_yaw = math.atan2(pose_in_target_frame.pose.position.y,
+                                            pose_in_target_frame.pose.position.x)
+
+                    rospy.loginfo(f"distance: {distance:.2f}m, target_yaw: {target_yaw:.2f}rad")
+
+                    category = self.get_valid_category(target_yaw)
+                    if category:
+                        rospy.loginfo(f"Motion category: {category.name}")
+                        self.publish_direction_marker(category.color, category.yaw)
+                        break
+            else:
+                rospy.loginfo(
+                    "No pose found in the path which is more than the threshold distance away "
+                    f"({self.distance_threshold_m:.2f}m)"
+                )
 
     @staticmethod
     def get_motion_categories(angle_threshold_rad) -> List[MotionCategory]:
@@ -131,12 +164,12 @@ class MotionMonitor():
         return math.sqrt(pose.position.x**2 + pose.position.y**2)
 
     @staticmethod
-    def get_direction_marker(frame_id: str, scale: Vector3 = Vector3(1.5, 0.2, 0.2)):
+    def get_direction_marker(frame_id: str, scale: Vector3, positon: Point):
         marker = Marker()
         marker.header.frame_id = frame_id
         marker.type = Marker.ARROW
         marker.action = Marker.ADD
-        marker.pose.orientation.z = 0.1
+        marker.pose.position = positon
         marker.pose.orientation.w = 1.0
         marker.scale = scale
         return marker
